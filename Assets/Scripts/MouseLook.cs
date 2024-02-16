64bff27c86b8c82b26cb3a945fa25bec9d9d3687
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-02-16 16:17:36 +0300

    Add a look back behavior

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,12 +8,19 @@ public class MouseLook : MonoBehaviour
 	private Vector2 _turn;
 	private float _xRotation;
 
-	void Start() {
+	void Start()
+	{
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 
-    void Update()
-    {
+	void Update()
+	{
+		MouseLookingLogic();
+		LookAroundLogic();
+	}
+
+	void MouseLookingLogic()
+	{
 		_turn.x = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
 		_turn.y = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
 
@@ -22,5 +29,13 @@ public class MouseLook : MonoBehaviour
 
 		transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
 		_playerBody.Rotate(Vector3.up * _turn.x);
-    }
+	}
+
+	void LookAroundLogic()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.localRotation = Quaternion.Euler(_xRotation, 180f, 0f);
+		}
+	}
 }
