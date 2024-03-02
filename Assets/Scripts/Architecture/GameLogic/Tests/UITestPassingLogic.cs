c8b5bdfc13b71f1672de0c9081f8e166b818782c
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-03-02 08:12:20 +0300

    Add coloured btns and test passing logic

--- Assets/Scripts/Architecture/GameLogic/Tests/UITestPassingLogic.cs
+++ Assets/Scripts/Architecture/GameLogic/Tests/UITestPassingLogic.cs
@@ -4,40 +4,148 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UITestPassingLogic : MonoBehaviour
 {
 	[SerializeField] private GameObject _testCanvas;
+	private int _currentTestNmb;
+	private int _previousTestNmb;
+	private Color _previousBtnColor;
 	private Test _test;
+	private List<int> _notAnsweredTestNumbers;
 
 	[SerializeField] private TextMeshProUGUI _questionText;
-	[SerializeField] private GameObject _answerParent;
+	[SerializeField] private GameObject _answersParent;
+	private ToggleGroup _answersParentToggleGroup;
 	[SerializeField] private GameObject _testNumbersParent;
-	private ToggleGroup _answerParentToggleGroup;
 
 	[SerializeField] private GameObject _answerPrefab;
 	[SerializeField] private GameObject _testNumberPrefab;
 
+	[SerializeField] private TextMeshProUGUI _scoreText;
+	[SerializeField] private Button _acceptBtn;
+
+	private const int TO_PERSENTS = 100;
+
 	public void InitialSetup(Test test)
 	{
+		// if (!test.IsReplayable) {
+		// 	return;
+		// }
+
+		_test = test;
+		_test.Reset();
+
+		_testCanvas.SetActive(true);
+
 		Cursor.lockState = CursorLockMode.None;
 
-		_answerParentToggleGroup = _answerParent.GetComponent<ToggleGroup>();
-		_test = test;
-		_testCanvas.SetActive(true);
-		LoadTestItem(0);
+		_currentTestNmb = _previousTestNmb = 0;
+
+		_answersParentToggleGroup = _answersParent.GetComponent<ToggleGroup>();
+
+		if (_testNumbersParent.transform.childCount > 0)
+		{
+			DestroyChildrens(_testNumbersParent.transform);
+		}
+
 		LoadTestNumbers();
+		LoadTestItem(_currentTestNmb);
+
+		_acceptBtn.onClick.AddListener(OnAcceptButtonClicked);
+
+		_notAnsweredTestNumbers = new List<int>();
+		_notAnsweredTestNumbers = Enumerable.Range(0, _test.NumberOfQuestions).ToList();
 	}
 
-	public void TestPassed() {
+	public IEnumerator TestPassed()
+	{
+		yield return new WaitForSeconds(1.5f);
+
 		Cursor.lockState = CursorLockMode.Locked;
+		_testCanvas.SetActive(false);
+	}
+
+	public void OnAcceptButtonClicked()
+	{
+		int correctlyAnswered = 0;
+		_notAnsweredTestNumbers.Remove(_currentTestNmb);
+
+		foreach (Transform tr in _answersParent.transform)
+		{
+			Toggle answerToggle = tr.GetComponent<Toggle>();
+
+			if (!answerToggle.isOn)
+			{
+				continue;
+			}
+
+			if (_test.TestItems[_currentTestNmb].correctAnswers.Contains(Convert.ToInt32(answerToggle.name)))
+			{
+				_test.CorrectlyAnsweredQuestionAnswers += 1;
+				correctlyAnswered += 1;
+			}
+		}
+
+		_scoreText.text = Math.Round(Utility.GetPercentage(_test.CorrectlyAnsweredQuestionAnswers, _test.TotalNumberOfCorrectAnswersOfQuestions) * TO_PERSENTS).ToString() + " %";
+
+		Transform selectedTestNumberTransform = _testNumbersParent.transform.Find(_currentTestNmb.ToString());
+		if (selectedTestNumberTransform != null)
+		{
+			Button selectedTestNumber = selectedTestNumberTransform.gameObject.GetComponent<Button>();
+			Image selectedTestNumberImage = selectedTestNumber.GetComponent<Image>();
+
+			if (correctlyAnswered == _test.TestItems[_currentTestNmb].correctAnswers.Count)
+			{
+				SetImageColor(selectedTestNumberImage, Color.green);
+				_previousBtnColor = selectedTestNumberImage.color;
+			}
+			else if (correctlyAnswered != 0)
+			{
+				SetImageColor(selectedTestNumberImage, Color.yellow);
+				_previousBtnColor = selectedTestNumberImage.color;
+			}
+			else if (correctlyAnswered == 0)
+			{
+				SetImageColor(selectedTestNumberImage, Color.red);
+				_previousBtnColor = selectedTestNumberImage.color;
+			} else {
+				_previousBtnColor = Color.white;
+			}
+
+			selectedTestNumber.interactable = false;
+		}
+		else
+		{
+			Debug.Log($"Can't find {_currentTestNmb} button.");
+		}
+
+		if (_notAnsweredTestNumbers.Count != 0)
+		{
+			LoadTestItem(_notAnsweredTestNumbers[0]);
+		}
+		else
+		{
+			StartCoroutine(TestPassed());
+		}
+	}
+
+	private void SetImageColor(Image image, in Color color)
+	{
+		image.color = color;
 	}
 
 	private void LoadTestItem(int testNmb)
 	{
-		RemovePreviousTestItem();
+		_previousTestNmb = _currentTestNmb;
+
+		if (_answersParent.transform.childCount > 0)
+		{
+			DestroyChildrens(_answersParent.transform);
+		}
 		_questionText.text = $"Вопрос ({testNmb + 1}) - " + _test.TestItems[testNmb].question;
 
 		if (_test.TestItems[testNmb].correctAnswers.Count == 1)
@@ -48,29 +156,40 @@ public class UITestPassingLogic : MonoBehaviour
 		{
 			SetCheckBoxAnswers(_test.TestItems[testNmb]);
 		}
+
+
+		SetImageColor(_testNumbersParent.transform.Find(_previousTestNmb.ToString())?.gameObject.GetComponent<Image>(), _previousBtnColor);
+		Image selectedTestNumberImage = _testNumbersParent.transform.Find(testNmb.ToString())?.gameObject.GetComponent<Image>();
+		if (selectedTestNumberImage != null)
+		{
+			SetImageColor(selectedTestNumberImage, Color.cyan);
+		}
+
+		_currentTestNmb = testNmb;
+
+		_previousBtnColor = Color.white;
 	}
 
-	private void RemovePreviousTestItem() {
-		DestroyChildrens(_answerParent.transform);
-	}
-
-	private void DestroyChildrens(Transform transform) {
-		foreach (Transform tr in transform) {
+	private void DestroyChildrens(Transform transform)
+	{
+		foreach (Transform tr in transform)
+		{
 			Destroy(tr.gameObject);
 		}
 	}
 
 	private void SetRadioButtonAnswers(in Test.TestItem testItem)
 	{
-		_answerParentToggleGroup.enabled = true;
+		_answersParentToggleGroup.enabled = true;
 
 		for (int i = 0; i < testItem.answers.Count; ++i)
 		{
 			GameObject answerPrefab = Instantiate(_answerPrefab);
-			answerPrefab.transform.SetParent(_answerParent.transform, false);
+			answerPrefab.transform.SetParent(_answersParent.transform, false);
+			answerPrefab.name = i.ToString();
 
 			Toggle answerPrefabToggle = answerPrefab.GetComponent<Toggle>();
-			answerPrefabToggle.group = _answerParentToggleGroup;
+			answerPrefabToggle.group = _answersParentToggleGroup;
 			answerPrefabToggle.isOn = false;
 
 			answerPrefab.GetComponentInChildren<TextMeshProUGUI>().text = testItem.answers[i];
@@ -79,18 +198,20 @@ public class UITestPassingLogic : MonoBehaviour
 
 	private void SetCheckBoxAnswers(in Test.TestItem testItem)
 	{
-		_answerParentToggleGroup.enabled = false;
+		_answersParentToggleGroup.enabled = false;
 
 		for (int i = 0; i < testItem.answers.Count; ++i)
 		{
-			GameObject answerPrefab = Instantiate(_answerPrefab, _answerParent.transform, false);
+			GameObject answerPrefab = Instantiate(_answerPrefab, _answersParent.transform, false);
 			answerPrefab.GetComponentInChildren<TextMeshProUGUI>().text = testItem.answers[i];
+			answerPrefab.name = i.ToString();
 		}
 	}
 
 	private void LoadTestNumbers()
 	{
-		for (int i = 0; i < _test.NumberOfQuestions; ++i) {
+		for (int i = 0; i < _test.NumberOfQuestions; ++i)
+		{
 			GameObject testNumberPrefab = Instantiate(_testNumberPrefab);
 			testNumberPrefab.transform.SetParent(_testNumbersParent.transform, false);
 			testNumberPrefab.name = i.ToString();
