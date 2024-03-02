ec7a8afad86233e479041a7cbdb77074d8089d5b
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-03-02 09:06:14 +0300

    Optimize StopGameLogic script

--- Assets/Scripts/Architecture/GameLogic/StopGameLogic.cs
+++ Assets/Scripts/Architecture/GameLogic/StopGameLogic.cs
@@ -1,53 +1,60 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class StopGameLogic : MonoBehaviour
 {
 	private static Dictionary<IMovable, float> _baseSpeed;
+	private static IMovable[] _movableCharacters;
 
 	static StopGameLogic()
 	{
 		_baseSpeed = new Dictionary<IMovable, float>();
+		GameObject[] _characters = FindGameObjectsWithLayer(LayerMask.NameToLayer("CharacterLayer"));
+
+		_movableCharacters = new IMovable[_characters.Length];
+		for (int i = 0; i < _characters.Length; ++i)
+		{
+			IMovable movableCharacter = _characters[i].GetComponent<IMovable>();
+			_movableCharacters[i] = movableCharacter;
+
+			_baseSpeed.Add(_movableCharacters[i], _movableCharacters[i].Speed);
+		}
 	}
 
 	public static void StopGame()
 	{
-		foreach (GameObject character in FindGameObjectsWithLayer(LayerMask.NameToLayer("CharacterLayer")))
+		foreach (IMovable movableCharacter in _movableCharacters)
 		{
-			IMovable movableCharacter = character.GetComponent<IMovable>();
-			_baseSpeed.Add(movableCharacter, movableCharacter.Speed);
 			movableCharacter.Speed = 0;
 		}
 	}
 
 	public static void ResumeGame()
 	{
-		foreach (GameObject character in FindGameObjectsWithLayer(LayerMask.NameToLayer("CharacterLayer")))
+		foreach (IMovable movableCharacter in _movableCharacters)
 		{
-			IMovable movableCharacter = character.GetComponent<IMovable>();
 			movableCharacter.Speed = _baseSpeed[movableCharacter];
 		}
 	}
 
-	private static GameObject[] FindGameObjectsWithLayer(LayerMask searchLayer) {
-    	GameObject[] goArray = (GameObject[]) FindObjectsOfType(typeof(GameObject));
-    	var goList = new List<GameObject>();
+	private static GameObject[] FindGameObjectsWithLayer(LayerMask searchLayer)
+	{
+		GameObject[] goArray = (GameObject[])FindObjectsOfType(typeof(GameObject));
+		var goList = new List<GameObject>();
 
-    	for (var i = 0; i < goArray.Length; i++) {
-			if (goArray[i].layer == searchLayer) {
+		for (var i = 0; i < goArray.Length; i++)
+		{
+			if (goArray[i].layer == searchLayer)
+			{
 				goList.Add(goArray[i]);
-			}
- 		}
-
-		if (goList.Count == 0) {
-			return null;
-		} else {
-			foreach (var c in goList) {
-				Debug.Log($"Character name: {c.name}");
 			}
 		}
 
+		if (goList.Count == 0)
+		{
+			return null;
+		}
+
 		return goList.ToArray();
- 	}
+	}
 }
