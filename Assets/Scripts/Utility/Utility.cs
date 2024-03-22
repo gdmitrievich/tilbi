a5ae6a6874bc27e6897f73c46b67d89ea97ea26f
MikhailovDev <555lifeisbeautiful5@gmail.com>
2024-03-22 16:06:07 +0300

    Add Utility.ExtractRandomElementFromList method

--- Assets/Scripts/Utility/Utility.cs
+++ Assets/Scripts/Utility/Utility.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -35,5 +36,32 @@ public class Utility : MonoBehaviour
 		{
 			Destroy(tr.gameObject);
 		}
+	}
+
+	public static T ExtractRandomElementFromList<T>(List<T> list) where T : ICloneable
+	{
+		int idx = UnityEngine.Random.Range(0, list.Count);
+		T elem = (T)list[idx].Clone();
+		list.RemoveAt(idx);
+
+		return elem;
+	}
+
+	public static int ExtractRandomElementFromList(List<int> list)
+	{
+		int idx = UnityEngine.Random.Range(0, list.Count);
+		int elem = list[idx];
+		list.RemoveAt(idx);
+
+		return elem;
+	}
+
+	public static T ExtractRandomElementFromList<T>(List<T> list, out int idx) where T : ICloneable
+	{
+		idx = UnityEngine.Random.Range(0, list.Count);
+		T elem = (T)list[idx].Clone();
+		list.RemoveAt(idx);
+
+		return elem;
 	}
 }
