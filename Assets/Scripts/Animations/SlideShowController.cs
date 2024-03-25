c98ae130071cbb83cdbe50aebc1762fd39d8caf6
MikhailovDev <555lifeisbeautiful5@gmail.com>
2024-03-25 07:11:01 +0300

    Add SlideShowController script

---
+++ Assets/Scripts/Animations/SlideShowController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
+
+public class SlideShowController : MonoBehaviour
+{
+	[SerializeField] private float _timeToSee;
+	[SerializeField] private float _transitionTime;
+	[SerializeField] private Sprite[] _sprites;
+
+	private Image _currentImage;
+	private Image _nextImage;
+
+	private int _spritesCounter;
+	private float _time;
+
+	private bool _isGoing;
+
+	void Awake()
+	{
+		_currentImage = transform.Find("Current Image").GetComponent<Image>();
+		_nextImage = transform.Find("Next Image").GetComponent<Image>();
+
+		_currentImage.sprite = _sprites[0];
+		_nextImage.sprite = _sprites[1];
+
+		_time = 0;
+		_spritesCounter = 0;
+
+		_isGoing = false;
+		Color col = _currentImage.color;
+		col.a = 100;
+		_currentImage.color = col;
+	}
+
+	void Update()
+	{
+		if (!_isGoing)
+		{
+			StartCoroutine(ShowPicture());
+		}
+	}
+
+	private IEnumerator ShowPicture()
+	{
+		_isGoing = true;
+		yield return new WaitForSeconds(_timeToSee);
+
+		while (_time < _transitionTime)
+		{
+			_time += Time.deltaTime;
+
+			_currentImage.color = new Color(_currentImage.color.r, _currentImage.color.g, _currentImage.color.b, 1 - _time / _transitionTime);
+
+			yield return null;
+		}
+
+		_time = 0;
+
+		_spritesCounter++;
+		if (_spritesCounter < _sprites.Length - 1) {
+			_currentImage.color = new Color(_currentImage.color.r, _currentImage.color.g, _currentImage.color.b, 1);
+			_currentImage.sprite = _sprites[_spritesCounter];
+			_nextImage.sprite = _sprites[_spritesCounter + 1];
+			_isGoing = false;
+		}
+
+		yield break;
+	}
+}
