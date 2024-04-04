cccacac41d437d19bd7680954a993b4c0a9bff05
MikhailovDev <555lifeisbeautiful5@gmail.com>
2024-04-04 19:13:15 +0300

    Add main hero heartbeat effect

--- Assets/Scripts/Architecture/GameLogic/MainHeroEffectsController.cs
+++ Assets/Scripts/Architecture/GameLogic/MainHeroEffectsController.cs
@@ -2,7 +2,11 @@ using System;
 using TMPro;
 using UnityEngine;
 
-public class MainHeroLightController : MonoBehaviour {
+public class MainHeroEffectsController : MonoBehaviour {
+	[SerializeField] private float _maxHeartBeatSpeed;
+	private AudioSource _heartBeatAudioSource;
+	private float _initialHeartBeatVolume;
+
 	[SerializeField] private Color _initColor;
 	[SerializeField] private Color _targetColor;
 
@@ -16,13 +20,20 @@ public class MainHeroLightController : MonoBehaviour {
 	private Transform _tilbiTransform;
 
 	void Awake () {
-		_light = GetComponent<Light>();
+		_heartBeatAudioSource = GetComponentsInChildren<AudioSource>()[1];
+		_heartBeatAudioSource.Play();
+		_initialHeartBeatVolume = _heartBeatAudioSource.volume;
+
+		_light = GetComponentInChildren<Light>();
 
 		_tilbiTransform = GameObject.FindGameObjectWithTag("Tilbi").transform;
 	}
 
 	void Update() {
 		_distance = Math.Clamp(Vector3.Distance(transform.position, _tilbiTransform.position), 0, _maxDistance);
+
+		_heartBeatAudioSource.volume = (float) Math.Sqrt(1 - _distance / _maxDistance) * _initialHeartBeatVolume;
+		_heartBeatAudioSource.pitch = _maxHeartBeatSpeed * (float) Math.Sqrt(1 - _distance / _maxDistance);
 
 		_light.range = _initLightRange + (_targetLightRange - _initLightRange) * Utility.GetPercentage(_maxDistance - _distance, _maxDistance);
 		_light.color = _initColor + (_targetColor - _initColor) * Utility.GetPercentage(_maxDistance - _distance, _maxDistance);
